0f5fb9c5c9f480a91c79fe0a06b0976e3b7c153a
Marcos Judsson <Marcos Judsson>
2017-04-07 21:45:43 -0300

    Refatoração

--- HappyNumber/HappyNumber.java
+++ HappyNumber/HappyNumber.java
@@ -1,24 +1,24 @@
 
 public class HappyNumber {
 
-	public String test(int i){
-		if (i==10){
-			
+	public String test(int i) {
+		if (i == 10) {
+			int soma = 0;
 			String digitos = String.valueOf(i);
-			
-			int dig1 = Integer.parseInt(String.valueOf(digitos.charAt(0)));
-			int soma = dig1;
-			
-			int dig2 = Integer.parseInt(String.valueOf(digitos.charAt(1)));
-			soma = soma + dig2;
-			
-			if(soma == 1){
-			return "feliz";
-			}
-			}
 
-		if (i==1 || i==100 || i==1000){
-		return "feliz";
+			int dig1 = Integer.parseInt(String.valueOf(digitos.charAt(0)));
+			soma += dig1 * dig1;
+
+			int dig2 = Integer.parseInt(String.valueOf(digitos.charAt(1)));
+			soma += dig2 * dig2;
+
+			if (soma == 1) {
+				return "feliz";
+			}
+		}
+
+		if (i == 1 || i == 100 || i == 1000) {
+			return "feliz";
 		}
 		return "infeliz";
 	}
