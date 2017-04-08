4dfd3d306aed4133db41c159cb49e5a82e9f249d
Marcos Judsson <Marcos Judsson>
2017-04-07 21:39:38 -0300

    Criado calculo numero 10 e validação

--- HappyNumber/HappyNumber.java
+++ HappyNumber/HappyNumber.java
@@ -2,8 +2,22 @@
 public class HappyNumber {
 
 	public String test(int i){
-		
-		if (i==1|| i==10 || i==100 || i==1000){
+		if (i==10){
+			
+			String digitos = String.valueOf(i);
+			
+			int dig1 = Integer.parseInt(String.valueOf(digitos.charAt(0)));
+			int soma = dig1;
+			
+			int dig2 = Integer.parseInt(String.valueOf(digitos.charAt(1)));
+			soma = soma + dig2;
+			
+			if(soma == 1){
+			return "feliz";
+			}
+			}
+
+		if (i==1 || i==100 || i==1000){
 		return "feliz";
 		}
 		return "infeliz";
