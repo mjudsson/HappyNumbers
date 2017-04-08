d84292179d5afce278000e701a07bc0c2654e4b3
Marcos Judsson <Marcos Judsson>
2017-04-07 21:01:44 -0300

    Signed-off-by: Marcos Judsson <Marcos Judsson>

---
+++ HappyNumber/HappyNumber.java
@@ -0,0 +1,11 @@
+
+public class HappyNumber {
+
+	public String test(int i){
+		
+		if (i==1|| i==10 || i==100 || i==1000){
+		return "feliz";
+		}
+		return "infeliz";
+	}
+}
---
+++ test/HappyNumberTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HappyNumberTest {
+
+	@Test
+	public void testaNumero1() {
+		HappyNumber happyNumber = new HappyNumber();
+		assertEquals("feliz", happyNumber.test(1));
+	}
+	@Test
+	public void testaNumero7() {
+		HappyNumber happyNumber = new HappyNumber();
+		assertEquals("infeliz", happyNumber.test(7));
+	}
+	@Test
+	public void testaNumero10() {
+		HappyNumber happyNumber = new HappyNumber();
+		assertEquals("feliz", happyNumber.test(10));
+	}
+	@Test
+	public void testaNumero100() {
+		HappyNumber happyNumber = new HappyNumber();
+		assertEquals("feliz", happyNumber.test(100));
+	}
+	@Test
+	public void testaNumero1000() {
+		HappyNumber happyNumber = new HappyNumber();
+		assertEquals("feliz", happyNumber.test(1000));
+	}
+}
