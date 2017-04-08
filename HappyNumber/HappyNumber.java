53b3b4a3c8afdeafef07ab5ba4be1817edcc1b61
Marcos Judsson <Marcos Judsson>
2017-04-07 21:52:20 -0300

    Iniciado FOR para numero 10

--- HappyNumber/HappyNumber.java
+++ HappyNumber/HappyNumber.java
@@ -3,15 +3,14 @@ public class HappyNumber {
 
 	public String test(int i) {
 		if (i == 10) {
-			int soma = 0;
 			String digitos = String.valueOf(i);
+			int soma = 0;
+			for (int cont = 0; cont < digitos.length(); cont++) {
 
-			int dig1 = Integer.parseInt(String.valueOf(digitos.charAt(0)));
-			soma += dig1 * dig1;
+				int dig = Integer.parseInt(String.valueOf(digitos.charAt(cont)));
+				soma += dig * dig;
 
-			int dig2 = Integer.parseInt(String.valueOf(digitos.charAt(1)));
-			soma += dig2 * dig2;
-
+			}
 			if (soma == 1) {
 				return "feliz";
 			}
